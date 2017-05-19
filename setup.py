c9eb02b5007fd05679ab17bab0ecd9449e550224
Tzung-Bi Shih <penvirus@gmail.com>
2017-05-19 15:17:00 +0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+	name = "macho_parser",
+	version = "1.0.0",
+	author = "Tzung-Bi Shih",
+	author_email = "penvirus@gmail.com",
+	description = "a simple macho parser",
+	license = "BSD",
+	keywords = "macho parser",
+    url = "https://github.com/penvirus/macho_parser",
+	packages= ["macho_parser"],
+	long_description= "a simple macho parser",
+	classifiers= [
+		"Development Status :: 3 - Alpha",
+        "Environment :: Console",
+		"License :: OSI Approved :: BSD License",
+        "Programming Language :: Python",
+		"Topic :: Utilities",
+    ],
+)
